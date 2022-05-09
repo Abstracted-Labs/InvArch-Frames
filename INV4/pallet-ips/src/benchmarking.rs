a14775d81dc07ed54f04ec743886396ab3fb6ffa
Kresna Sucandra <kresnasucandra@gmail.com>
2022-05-09 18:23:39 +0800

    Chore: create IPS benchmarking.rs placeholder

---
+++ INV4/pallet-ips/src/benchmarking.rs
@@ -0,0 +1,56 @@
+//! Benchmarks for IPS Pallet
+#![cfg(feature = "runtime-benchmarks")]
+
+use crate::*;
+use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, whitelisted_caller};
+use frame_system::RawOrigin;
+
+benchmarks!{
+  create_ips {
+    let s in 0 .. 100;
+    let caller: T::AccountId = whitelisted_caller();
+}: _(RawOrigin::Signed(caller), s)
+verify {}
+
+destroy {
+    let s in 0 .. 100;
+    let caller: T::AccountId = whitelisted_caller();
+}: _(RawOrigin::Signed(caller), s)
+verify {}
+
+append {
+    let s in 0 .. 100;
+    let caller: T::AccountId = whitelisted_caller();
+}: _(RawOrigin::Signed(caller), s)
+verify {}
+
+remove {
+    let s in 0 .. 100;
+    let caller: T::AccountId = whitelisted_caller();
+}: _(RawOrigin::Signed(caller), s)
+verify {}
+
+allow_replica {
+    let s in 0 .. 100;
+    let caller: T::AccountId = whitelisted_caller();
+}: _(RawOrigin::Signed(caller), s)
+verify {}
+
+disallow_replica {
+    let s in 0 .. 100;
+    let caller: T::AccountId = whitelisted_caller();
+}: _(RawOrigin::Signed(caller), s)
+verify {}
+
+create_replica {
+    let s in 0 .. 100;
+    let caller: T::AccountId = whitelisted_caller();
+}: _(RawOrigin::Signed(caller), s)
+verify {}
+}
+
+impl_benchmark_test_suite!(
+  YourPallet,
+  crate::mock::new_test_ext(),
+  crate::mock::Test,
+);
