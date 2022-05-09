0c8dc92ca737aeddec640e13ec23c4aafc93a0c6
Kresna Sucandra <kresnasucandra@gmail.com>
2022-05-09 20:02:23 +0800

    Chore: create IPF benchmarking.rs placeholder

---
+++ INV4/pallet-ipf/src/benchmarking.rs
@@ -0,0 +1,28 @@
+//! Benchmarks for IPF Pallet
+#![cfg(feature = "runtime-benchmarks")]
+
+use crate::*;
+use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, whitelisted_caller};
+use frame_system::RawOrigin;
+
+benchmarks! {
+  mint {
+    let s in 0 .. 100;
+    let caller: T::AccountId = whitelisted_caller();
+  }: _(RawOrigin::Signed(caller), s)
+  verify {}
+
+  burn {
+      let s in 0 .. 100;
+      let caller: T::AccountId = whitelisted_caller();
+  }: _(RawOrigin::Signed(caller), s)
+  verify {}
+
+  send {
+      let s in 0 .. 100;
+      let caller: T::AccountId = whitelisted_caller();
+  }: _(RawOrigin::Signed(caller), s)
+  verify {}
+}
+
+impl_benchmark_test_suite!(YourPallet, crate::mock::new_test_ext(), crate::mock::Test,);
