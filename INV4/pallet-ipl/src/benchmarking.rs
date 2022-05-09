70523f1addda6ea72f12ddf416183755e0c4da06
Kresna Sucandra <kresnasucandra@gmail.com>
2022-05-09 20:06:07 +0800

    Chore: create IPL benchmarking.rs placeholder

---
+++ INV4/pallet-ipl/src/benchmarking.rs
@@ -0,0 +1,22 @@
+//! Benchmarks for IPL Pallet
+#![cfg(feature = "runtime-benchmarks")]
+
+use crate::*;
+use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, whitelisted_caller};
+use frame_system::RawOrigin;
+
+benchmarks! {
+  set_permission {
+    let s in 0 .. 100;
+    let caller: T::AccountId = whitelisted_caller();
+  }: _(RawOrigin::Signed(caller), s)
+  verify {}
+
+  set_asset_weight {
+      let s in 0 .. 100;
+      let caller: T::AccountId = whitelisted_caller();
+  }: _(RawOrigin::Signed(caller), s)
+  verify {}
+}
+
+impl_benchmark_test_suite!(YourPallet, crate::mock::new_test_ext(), crate::mock::Test,);
