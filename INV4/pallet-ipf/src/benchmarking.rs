8fedfc917eda11f3872d549e5a5a8b1ed34d3499
Kresna Sucandra <kresnasucandra@gmail.com>
2022-05-16 00:53:51 +0800

    Update IPF benchmarking

--- INV4/pallet-ipf/src/benchmarking.rs
+++ INV4/pallet-ipf/src/benchmarking.rs
@@ -1,28 +1,33 @@
 //! Benchmarks for IPF Pallet
 #![cfg(feature = "runtime-benchmarks")]
 
-use crate::*;
-use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, whitelisted_caller};
-use frame_system::RawOrigin;
+use crate::IpfInfo;
+use frame_benchmarking::{account, benchmarks, impl_benchmark_test_suite, whitelisted_caller};
+use frame_system::{RawOrigin, Config, Pallet};
+use sp_runtime::traits::StaticLookup;
+
+const SEED: u32 = 0;
 
 benchmarks! {
+  // Mint IPF
   mint {
-    let s in 0 .. 100;
-    let caller: T::AccountId = whitelisted_caller();
-  }: _(RawOrigin::Signed(caller), s, _)
-  verify {}
+        let caller: T::AccountId = whitelisted_caller();
+        let metadata: Vec<u8> = vec![1],
+        let data = H256
+    }: _(RawOrigin::Signed(caller), metadata, data)
 
-  burn {
-      let s in 0 .. 100;
-      let caller: T::AccountId = whitelisted_caller();
-  }: _(RawOrigin::Signed(caller), s)
-  verify {}
+//   // Burn IPF
+//   burn {
+//     let caller: T::AccountId = whitelisted_caller();
+//     let ipf_id: u64 = 0,
+// }: _(RawOrigin::Signed(caller), ipf_id)
 
-  send {
-      let s in 0 .. 100;
-      let caller: T::AccountId = whitelisted_caller();
-  }: _(RawOrigin::Signed(caller), s)
-  verify {}
+//   // Send IPF
+//   send {
+//         let caller: T::AccountId = whitelisted_caller();
+//         let ipf_id: u64 = 0,
+//         let target = T::Lookup::unlookup(to);;
+//     }: _(RawOrigin::Signed(caller), ipf_id, target)
 }
 
 impl_benchmark_test_suite!(Ipf, crate::mock::new_test_ext(), crate::mock::Test,);
