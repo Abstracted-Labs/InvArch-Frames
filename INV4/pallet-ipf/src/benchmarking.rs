238a1f0d82e9c59a159dec0bbc6cb8499af41d42
S0raWasTaken <s0ra@duck.com>
2022-05-16 01:48:41 -0500

    Refactor `mint` benchmark in pallet-ipf

--- INV4/pallet-ipf/src/benchmarking.rs
+++ INV4/pallet-ipf/src/benchmarking.rs
@@ -4,16 +4,23 @@
 use super::*;
 use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, vec, whitelisted_caller};
 use frame_system::{Config, RawOrigin};
+use sp_core::H256;
+
+const MOCK_DATA: [u8; 32] = [
+    12, 47, 182, 72, 140, 51, 139, 219, 171, 74, 247, 18, 123, 28, 200, 236, 221, 85, 25, 12, 218,
+    0, 230, 247, 32, 73, 152, 66, 243, 27, 92, 95,
+];
 
 benchmarks! {
     where_clause {
-        where T: pallet::Config
+        where T: pallet::Config + frame_system::Config<Hash = H256>
     }
     // Mint IPF
     mint {
         let caller: T::AccountId = whitelisted_caller();
         let metadata: Vec<u8> = vec![1];
-    }: _(RawOrigin::Signed(caller), metadata, Default::default())
+        let data = H256::from(MOCK_DATA);
+    }: _(RawOrigin::Signed(caller), metadata, data)
 
     // Burn IPF
     burn {
