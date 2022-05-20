708815c9cddacdeaf9339fea7fd61d43098954ff
Kresna Sucandra <kresnasucandra@gmail.com>
2022-05-20 20:41:45 +0800

    Fix: refactor burn in IPF benchmarking

--- INV4/pallet-ipf/src/benchmarking.rs
+++ INV4/pallet-ipf/src/benchmarking.rs
@@ -5,6 +5,7 @@ pub use super::*;
 use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, vec, whitelisted_caller};
 use frame_system::{Config, RawOrigin};
 use sp_core::H256;
+use frame_support::traits::OriginTrait;
 
 const MOCK_DATA: [u8; 32] = [
     12, 47, 182, 72, 140, 51, 139, 219, 171, 74, 247, 18, 123, 28, 200, 236, 221, 85, 25, 12, 218,
@@ -25,8 +26,14 @@ benchmarks! {
     // Burn IPF
     burn {
         let caller: T::AccountId = whitelisted_caller();
+        let metadata: Vec<u8> = vec![1];
+        let data = H256::from(MOCK_DATA);
         let ipf_id: u32 = 0;
+
+        Pallet::<T>::mint(RawOrigin::Signed(caller.clone()).into(), metadata, data)?;
     }: _(RawOrigin::Signed(caller), T::IpfId::from(ipf_id))
+
+    
 }
 
 impl_benchmark_test_suite!(Ipf, crate::mock::new_test_ext(), crate::mock::Test);
