74e7e01a956175b427605eb3b396e71eb9f84ea2
Kresna Sucandra <kresnasucandra@gmail.com>
2022-06-23 09:48:59 +0800

    Some clean up

--- INV4/pallet-inv4/src/benchmarking.rs
+++ INV4/pallet-inv4/src/benchmarking.rs
@@ -4,7 +4,7 @@ pub use super::*;
 use frame_benchmarking::{account, benchmarks, impl_benchmark_test_suite, vec, whitelisted_caller};
 use frame_system::RawOrigin;
 use primitives::OneOrPercent::{One, ZeroPoint};
-// use sp_core::H256;
+use sp_core::H256;
 use sp_runtime::{traits::UniqueSaturatedInto, Percent};
 
 // const MOCK_DATA: [u8; 32] = [
@@ -27,48 +27,7 @@ pub const MOCK_METADATA: &'static [u8] = &[
 //     230, 247, 32, 73, 152, 66, 243, 27, 92, 95, 12,
 // ];
 
-// type Hash = sp_core::H256;
-
-// pub trait LicenseList {
-//     type IpfsHash: core::hash::Hash;
-//     type LicenseMetadata;
-
-//     fn get_hash_and_metadata(&self) -> (Self::LicenseMetadata, Self::IpfsHash);
-// }
-
-// #[derive(Debug, Clone, Encode, Decode, TypeInfo, Eq, PartialEq)]
-// pub enum InvArchLicenses {
-//     Apache2,
-//     GPLv3,
-//     Custom(Vec<u8>, Hash),
-// }
-
-// impl LicenseList for InvArchLicenses {
-//     type IpfsHash = Hash; // License IPFS hash.
-//     type LicenseMetadata = Vec<u8>; // License name.
-
-//     fn get_hash_and_metadata(&self) -> (Self::LicenseMetadata, Self::IpfsHash) {
-//         match self {
-//             InvArchLicenses::Apache2 => (
-//                 vec![65, 112, 97, 99, 104, 97, 32, 118, 50, 46, 48],
-//                 [
-//                     7, 57, 92, 251, 234, 183, 217, 144, 220, 196, 201, 132, 176, 249, 18, 224, 237,
-//                     201, 2, 113, 146, 78, 111, 152, 92, 71, 16, 228, 87, 39, 81, 142,
-//                 ]
-//                 .into(),
-//             ),
-//             InvArchLicenses::GPLv3 => (
-//                 vec![71, 78, 85, 32, 71, 80, 76, 32, 118, 51],
-//                 [
-//                     72, 7, 169, 24, 30, 7, 200, 69, 232, 27, 10, 138, 130, 253, 91, 158, 210, 95,
-//                     127, 37, 85, 41, 106, 136, 66, 116, 64, 35, 252, 195, 69, 253,
-//                 ]
-//                 .into(),
-//             ),
-//             InvArchLicenses::Custom(metadata, hash) => (metadata.clone(), *hash),
-//         }
-//     }
-// }
+type Hash = sp_core::H256;
 
 const SEED: u32 = 0;
 
@@ -85,18 +44,12 @@ macro_rules! percent {
     };
 }
 
-// pub enum InvArchLicenses {
-//   Apache2,
-//   GPLv3,
-//   Custom,
-// }
-
 benchmarks! {
   create_ips {
     let bob: T::AccountId = account("Bob", 0, SEED);
     let metadata_1: Vec<u8> = MOCK_METADATA.to_vec();
     let assets = vec![AnyIdOf::<T>::from(pallet::AnyId::RmrkCollection(0u32))];
-    let ipl_license = ipl::LicenseList; // TODO: rewrite
+    let ipl_license = ipl::LicenseList::GPLv3; // TODO: rewrite
     let base_currency_amount = dollar(1000);
 
     <T as pallet::Config>::Currency::make_free_balance_be(&bob, base_currency_amount.unique_saturated_into());
