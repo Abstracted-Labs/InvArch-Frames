6f878a07bbabf32b5702d1c772a9c0b13238d89a
Gabriel Facco de Arruda <arrudagates@gmail.com>
2022-12-15 17:09:51 -0500

    feat: Added internal initialize_set function

--- pallet-rules/src/lib.rs
+++ pallet-rules/src/lib.rs
@@ -17,8 +17,10 @@ extern crate alloc;
 #[frame_support::pallet]
 pub mod pallet {
     use super::*;
+    use core::fmt::Display;
     use frame_support::{dispatch::Dispatchable, pallet_prelude::*};
     use frame_system::pallet_prelude::*;
+    use sp_runtime::traits::AtLeast32BitUnsigned;
 
     #[pallet::config]
     pub trait Config: frame_system::Config {
@@ -26,6 +28,15 @@ pub mod pallet {
         type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
 
         type Call: Parameter + Dispatchable + Encode + Decode + Rule;
+
+        type RulesetId: Parameter
+            + Member
+            + AtLeast32BitUnsigned
+            + Default
+            + Copy
+            + Display
+            + MaxEncodedLen
+            + Clone;
     }
 
     #[pallet::pallet]
@@ -34,21 +45,40 @@ pub mod pallet {
 
     #[pallet::storage]
     #[pallet::getter(fn ruleset)]
-    pub type Ruleset<T: Config> =
-        StorageMap<_, Blake2_128Concat, u64, Box<<<T as pallet::Config>::Call as Rule>::RuleSet>>;
+    pub type Ruleset<T: Config> = StorageMap<
+        _,
+        Blake2_128Concat,
+        <T as pallet::Config>::RulesetId,
+        Box<<<T as pallet::Config>::Call as Rule>::RuleSet>,
+    >;
+
+    #[pallet::error]
+    pub enum Error<T> {
+        HashNotFound,
+        SetNotFound,
+        SetAlreadyExists,
+
+        Error,
+    }
+
+    #[pallet::event]
+    #[pallet::generate_deposit(pub(super) fn deposit_event)]
+    pub enum Event<T: Config> {
+        EvalResult { result: bool },
+    }
 
     #[pallet::call]
     impl<T: Config> Pallet<T> {
         #[pallet::weight(1_000_000_000)]
         pub fn new_rule(
             origin: OriginFor<T>,
-            id: u64,
+            id: <T as pallet::Config>::RulesetId,
             rule: <<T as Config>::Call as Rule>::CallRule,
         ) -> DispatchResult {
             let _ = ensure_signed(origin)?;
 
             Ruleset::<T>::try_mutate(id, |set| -> DispatchResult {
-                let mut old_set = set.take().ok_or(Error::<T>::MapNotFound)?;
+                let mut old_set = set.take().ok_or(Error::<T>::SetNotFound)?;
 
                 old_set.add_rule(rule);
 
@@ -61,7 +91,10 @@ pub mod pallet {
         }
 
         #[pallet::weight(1_000_000_000)]
-        pub fn initialize_rule_set(origin: OriginFor<T>, id: u64) -> DispatchResult {
+        pub fn initialize_rule_set(
+            origin: OriginFor<T>,
+            id: <T as pallet::Config>::RulesetId,
+        ) -> DispatchResult {
             let _ = ensure_signed(origin)?;
 
             Ruleset::<T>::try_mutate(id, |set| -> DispatchResult {
@@ -80,14 +113,14 @@ pub mod pallet {
         }
 
         #[pallet::weight(1_000_000_000)]
-        pub fn check_rule_set(
+        pub fn check_rule(
             origin: OriginFor<T>,
-            id: u64,
+            id: <T as pallet::Config>::RulesetId,
             call: Box<<T as Config>::Call>,
         ) -> DispatchResultWithPostInfo {
             let _ = ensure_signed(origin)?;
 
-            let set = Ruleset::<T>::get(&id).ok_or(Error::<T>::MapNotFound)?;
+            let set = Ruleset::<T>::get(&id).ok_or(Error::<T>::SetNotFound)?;
 
             Self::deposit_event(Event::EvalResult {
                 result: call.check_rule(&*set),
@@ -97,17 +130,19 @@ pub mod pallet {
         }
     }
 
-    #[pallet::error]
-    pub enum Error<T> {
-        HashNotFound,
-        MapNotFound,
+    impl<T: Config> Pallet<T> {
+        pub fn initialize_set(id: <T as pallet::Config>::RulesetId) -> DispatchResult {
+            Ruleset::<T>::try_mutate(id, |set| -> DispatchResult {
+                if set.take().is_some() {
+                    return Err(Error::<T>::SetAlreadyExists.into());
+                }
 
-        Error,
-    }
+                *set = Some(Box::new(
+                    <<T as pallet::Config>::Call as Rule>::RuleSet::new(),
+                ));
 
-    #[pallet::event]
-    #[pallet::generate_deposit(pub(super) fn deposit_event)]
-    pub enum Event<T: Config> {
-        EvalResult { result: bool },
+                Ok(())
+            })
+        }
     }
 }
