bed2864944eaaa1facf89d8b34b51bc982df7594
arrudagates <arrudagates@users.noreply.github.com>
2024-03-11 16:24:03 +0000

    deploy: 693850e264d2d07fefc6579a2f8eb80bbc878036

--- src-files.js
+++ src-files.js
@@ -1,8 +1,8 @@
-var srcIndex = JSON.parse('{\
-"invarch_primitives":["",[],["lib.rs"]],\
-"pallet_checked_inflation":["",[],["inflation.rs","lib.rs","weights.rs"]],\
-"pallet_inv4":["",[],["account_derivation.rs","dispatch.rs","fee_handling.rs","inv4_core.rs","lib.rs","lookup.rs","multisig.rs","origin.rs","voting.rs","weights.rs"]],\
-"pallet_ocif_staking":["",[],["lib.rs","primitives.rs","weights.rs"]],\
-"pallet_rings":["",[],["lib.rs","traits.rs","weights.rs"]]\
-}');
+var srcIndex = new Map(JSON.parse('[\
+["invarch_primitives",["",[],["lib.rs"]]],\
+["pallet_checked_inflation",["",[],["inflation.rs","lib.rs","weights.rs"]]],\
+["pallet_inv4",["",[],["account_derivation.rs","dispatch.rs","fee_handling.rs","inv4_core.rs","lib.rs","lookup.rs","multisig.rs","origin.rs","voting.rs","weights.rs"]]],\
+["pallet_ocif_staking",["",[],["lib.rs","primitives.rs","weights.rs"]]],\
+["pallet_rings",["",[],["lib.rs","traits.rs","weights.rs"]]]\
+]'));
 createSrcSidebar();
